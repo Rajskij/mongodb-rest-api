29aadda259c473aa08bea47644a2a6672c133bc2
Rajski <ihor.haidukov@gmail.com>
2025-06-22 17:15:56 -0400

    add custom field and validation

--- model/user.js
+++ model/user.js
@@ -1,13 +1,22 @@
 import mongoose from "mongoose";
 
 const user = new mongoose.Schema({
-    name: {
+    firstName: {
         type: String,
         required: true
     },
+    lastName: String,
     email: {
         type: String,
-        required: true
+        // custom message
+        required: [true, "Email is require"],
+        // custom validator
+        validate: {
+            validator: () => {
+                return /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(email);
+            },
+            message: 'Email is not a valid!'
+        }
     },
     role: {
         type: String,
@@ -16,6 +25,18 @@ const user = new mongoose.Schema({
     phone: {
         type: String,
         default: 'N/A'
+    },
+    // custom field
+    fullName: { 
+        type: String,
+        get: () => {
+            return `${firstName} ${lastName}`
+        },
+        set: (value) => {
+            const parts = value.split(' ');
+            this.firstName = parts[0];
+            this.lastName = parts[1];
+        }
     }
 });
 
